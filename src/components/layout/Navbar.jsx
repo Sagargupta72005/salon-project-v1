e5e00d2bd8aa6e94a8a9f0e44e41a2abe3dcfaf2
Sagargupta72005 <sagargupta72005@gamil.com>
2025-12-20 21:33:50 +0530

    v

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -121,8 +121,7 @@ transition={{
     >
       {/* MOBILE */}
       <div className="flex md:hidden justify-between items-center w-full px-6 py-4">
-        <Link onClick={() => setActiveLink(item)}
- to="/">
+        <Link to="/">
           <img
             src="https://cutstyle.true-emotions.studio/wp-content/themes/cutstyle/images/logo.svg"
             alt="Logo"
@@ -153,8 +152,7 @@ transition={{
 
       {/* TABLET */}
       <div className="hidden md:flex lg:hidden justify-between items-center w-full px-8 py-2">
-        <Link to="/" onClick={() => setActiveLink(item)}
->
+        <Link to="/">
           <img
             src="https://cutstyle.true-emotions.studio/wp-content/themes/cutstyle/images/logo.svg"
             alt="Logo"
@@ -305,8 +303,7 @@ transition={{
       <div className="hidden xl:flex justify-between py-4">
         <nav className="flex justify-between items-center text-xl font-light pl-20 gap-10">
           {menuItems.slice(0, middleIndex).map((item) => (
-            <Link onClick={() => setActiveLink(item)}
-
+            <Link
   to={getPath(item)}
   key={item}
   className={`cursor-pointer transition ${
@@ -322,8 +319,7 @@ transition={{
 
           ))}
 
-          <Link to="/" onClick={() => setActiveLink(item)}
->
+          <Link to="/">
             <img
               src="https://cutstyle.true-emotions.studio/wp-content/themes/cutstyle/images/logo.svg"
               alt="Logo"
@@ -416,7 +412,6 @@ transition={{
       <AnimatePresence>
         {mobileOpen && (
           <motion.div
-          onClick={() => setActiveLink(item)}
             initial={{ x: "-100%" }}
             animate={{ x: 0 }}
             exit={{ x: "-100%" }}
